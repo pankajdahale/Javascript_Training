2b781aeace3af2741887e8b4f6beb379c84f6f6b
pankaj <pankaj@idpsolutions.com.au>
2017-02-03 13:12:36 +0530

    Add project files.

---
+++ Javascript_Training/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Javascript_Training.MvcApplication" Language="C#" %>
---
+++ Javascript_Training/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Javascript_Training.Startup))]
+namespace Javascript_Training
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
